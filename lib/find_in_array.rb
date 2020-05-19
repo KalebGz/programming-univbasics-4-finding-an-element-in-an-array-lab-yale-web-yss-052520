cced7a018d084453169df9bbed928af323c3f1b1
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-19 06:06:48 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
   found = 0
   array.length.times {|index|
-    if(array[index] == value_to_find){
+    if(array[index] == value_to_find)
         found = 1
         break;
-      }
+    end
   }
   found == 1
 end
