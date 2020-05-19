fa0d15af2140de46fe67b32623a46099be4a061b
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-19 06:10:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
   found  = nil
   array.length.times {|index|
-    if(array[index] == value_to_find){
+    if(array[index] == value_to_find)
         found = index
         break;
-    }
+    end
   }
   found 
 end
